34e542d0859941e9721061e54f7121e42e606b17
Kärt <jogilakart33@gmail.com>
2023-11-12 15:07:37 +0200

    add week 7

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a word:");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); //userInput - argument
+
+static void PrintAnyWord(string anyString) //anyString - Parameter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
+
---
+++ FahrenheightAndCelsius/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("Enter temperature scale (Fahrenheit write f or Celsius write c):");
+char userChoice = Char.Parse(Console.ReadLine());
+
+if (userChoice == 'f')
+{
+    Console.WriteLine("Enter the temperature in Fahrenheit:")
+    double fahrenheit = Convert.ToDouble(Console.ReadLine());
+    ConvertToCelsius(double fahrenheit);
+}
+else if (userChoice == 'c')
+{
+    Console.WriteLine("Enter the temperature in Celsius:")
+    double celsius = Convert.ToDouble(Console.ReadLine());
+    ConvertToFahrenheit(double celsius);
+}
+else
+{
+    Console.WriteLine("Please use letters c or f.");
+}
+
+static void ConvertToCelsius(double fahrenheit)
+{
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    Console.WriteLine($"Celsius: {celsius}");
+}
+
+static void ConvertToFahrenheit(double celsius)
+{
+    double fahrenheit = (celsius * 9) / 5 + 32;
+    Console.WriteLine($"Fahrenheit: {fahrenheit}");
+}
